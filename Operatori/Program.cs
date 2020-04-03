753c2f1ab0aa6614896be56150f3c8ed88226981
unknown <patrik.torjanac@gmail.com>
2020-04-03 12:22:54 +0200

    patrik.torjanac@gmail.com

---
+++ Operatori/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Operatori
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
